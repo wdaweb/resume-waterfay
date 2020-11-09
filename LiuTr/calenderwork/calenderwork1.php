14d998291211cb3a0aa9c292850684bc458207b2
leonachen <waterfay0.0@gmail.com>
2020-11-10 04:46:31 +0800

    月曆更新

--- LiuTr/calenderwork/calenderwork1.php
+++ LiuTr/calenderwork/calenderwork1.php
@@ -282,10 +282,10 @@ echo "</table>";
 <div id="carouselExampleControls" class="carousel slide carousel-fade position-absolute" style="z-index:-10000;border-radius: 100%;" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://picsum.photos/700/700/?random=1" style="z-index:-10000;border-radius: 100%; opacity:0.4;"  class="d-block w-100" alt="...">
+      <img src="https://picsum.photos/700/700/?random=1" style="z-index:-10000;border-radius: 100%; opacity:0.3;"  class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="https://picsum.photos/700/700/?random=1" style="z-index:-10000;border-radius: 100%; opacity:0.4;"   class="d-block w-100" alt="...">
+      <img src="https://picsum.photos/700/700/?random=1" style="z-index:-10000;border-radius: 100%; opacity:0.3;"   class="d-block w-100" alt="...">
     </div>
   </div>
 
