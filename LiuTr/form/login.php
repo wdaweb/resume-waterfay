0b9f5e05c348ed64cc14e85d4cb9089346e3d4d2
LeonaChen <waterfay0.0@gmail.com>
2020-11-11 11:37:28 +0800

    2

--- LiuTr/form/login.php
+++ LiuTr/form/login.php
@@ -1,34 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>登入頁面</title>
-    <style>
-    body{
-        background:skyblue;
+<?php
+
+$users=[
+    ['acc'=>'mack','pw'=>'1234','type'=>'vip'],
+    ['acc'=>'judy','pw'=>'5678','type'=>'mem'],
+    ['acc'=>'andy','pw'=>'9999','type'=>'vip']
+];
+
+if(!empty($_POST['acc']) && !empty($_POST['pw'])){
+    $acc=$_POST['acc'];
+    $pw=$_POST['pw'];
+    $chk='';
+    foreach($users as $user){
+        if($user['acc']==$acc){
+            $chk=$user;  
+        }
     }
-    </style>
-</head>
-<body>
-<ul class="nav">
-  <li class="nav-item">
-    <a class="nav-link active" href="#">首頁</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">產品分類</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">關於我們</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">登入</a>
-  </li>
-</ul>
-    
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>   <!--不一定會用到-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
-</body>
-</html>
+
+    if(!empty($chk)){
+        if($chk['pw']==$pw){
+            switch($chk['type']){
+            case 'vip';
+                header("location:vip.php?name={$chk['acc']}");
+            break;
+            case 'mem':
+                header("location:mem.php?name={$chk['acc']}");
+            break;
+            }
+        }else{
+            header("location:error.php?e=2");
+        }
+    }else{
+        header("location:error.php?e=1");
+    }
+
+}else{
+    header("location:login.html");
+}
+
+?>
