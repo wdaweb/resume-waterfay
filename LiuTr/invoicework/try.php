298ea805867c230f746955461cdc2f7c592a617d
LeonaChen <waterfay0.0@gmail.com>
2020-12-02 14:41:34 +0800

    js

--- LiuTr/invoicework/try.php
+++ LiuTr/invoicework/try.php
@@ -1,39 +1,92 @@
-<?php 
-include_once "base.php";
-$Idcard='A22142182';
-$pw='12345678';
-$_SESSION['err1']=[];
-$_SESSION['err2']=[];
-$sum=0;
-$Idnames=['A'=>10,'I'=>34,'O'=>35,'B'=>11,'C'=>12,'D'=>13,
-'E'=>14,'F'=>15,'G'=>16,'H'=>17,'J'=>18,'K'=>82,
-'L'=>20,'M'=>21,'N'=>22,'P'=>23,'Q'=>24,'R'=>25,
-'S'=>26,'T'=>65,'U'=>28,'V'=>29,'W'=>32,'X'=>30,
-'Y'=>31,'Z'=>33];
+<?php
+$dsn="mysql:host=localhost;dbname=students;charset=utf8";
+$pdo=new PDO($dsn,'root','');
 
-if(!empty($Idcard) && (!preg_match('/^[A-Za-z]{1}[1-2]{1}[0-9]{8}$/',$Idcard))){
-          echo "身分證格式錯誤";
-            }else{
-           $Idend=((str_split(substr($Idcard,8,9),1))[0])+ ((str_split(substr($Idcard,8,9),1))[1]);
-           foreach($Idnames as $Idname =>$value){
-             if($Idname==((str_split($Idcard,1)[0]))){
-               $Idhead=(str_split($value,1)[0])*1+(str_split($value,1)[1])*9;
-             }
-        }
-        for($i=0;$i<7;$i++){
-            $sum= $sum + ((str_split(substr($Idcard,1,7),1))[$i])*(8-$i);
-          }
-          if(($Idend+$sum+$Idhead)%10==0){
-             echo "身分證正確";
-          }else{
-             echo "身分證格式錯誤";
-          }
-      }
-    
-     if(preg_match('/^(?=.*[A-Za-z])(?=.*\d\S)[A-Za-z\d]{4,8}$/',$pw)) {
-         $pw=$pw;
-     } else {
-         $pw=$_SESSION['err2'];
-         echo $_SESSION['err2']='密碼格式錯誤';
-     }
+$wherelist=[];
+$urlist=array();
+if(!empty($_GET['title']))
+{
+$wherelist[]=" title like '%".$_GET['title']."%'";
+$urllist[]="title=".$_GET['title'];
+  
+}
+if(!empty($_GET['keywords']))
+{
+$wherelist[]=" keywords like '%".$_GET['keywords']."%'";
+$urllist[]="keywords=".$_GET['keywords'];
+}if(!empty($_GET['author']))
+{
+$wherelist[]=" author like '%".$_GET['author']."%'";
+$urllist[]="author=".$_GET['author'];
+}
+$where="";
+if(count($wherelist)>0)
+{
+$where=" where ".implode(' and ',$wherelist);
+$url='&'.implode('&',$urllist);
+}
+//分頁的實現原理
+//1.獲取資料表中總記錄數
+$sql="select count(*) from news $where ";
+$result=mysql_query($sql);
+$totalnum=mysql_num_rows($result);
+//每頁顯示條數
+$pagesize=5;
+//總共有幾頁
+$maxpage=ceil($totalnum/$pagesize);
+$page=isset($_GET['page'])?$_GET['page']:1;
+if($page <1)
+{
+$page=1;
+}
+if($page>$maxpage)
+{
+$page=$maxpage;
+}
+$limit=" limit ".($page-1)*$pagesize.",$pagesize";
+$sql1="select * from students {$where} {$limit}";
+  
+//$sql1="select * from news {$where} {$limit}";
+$res=mysql_query($sql1);
+  
 ?>
+<form action="searchpage.php" method="get">
+標題：<input type="text" name="title" value="<?php echo $_GET['title']?>" size="8">
+關鍵字<input type="text" name="keywords" value="<?php echo $_GET['keywords']?>" size="8">
+作者：<input type="text" name="author" value="<?php echo $_GET['author']?>" size="8">
+ <input type="button" value="檢視全部" onclick="window.location='searchpage.php'">
+ <input type="submit" value="搜尋">
+</form>
+  
+<table border="1" width="1000" align="center">
+ <tr>
+ <td>編號</td>
+ <td>標題</td>
+ <td>關鍵字</td>
+ <td>作者</td>
+ <td>日期</td>
+ <td>內容</td>
+ </tr>
+<?php while($row= mysql_fetch_assoc($res)){?>
+<tr>
+ <td><?php echo $row['id'] ?></td>
+ <td><?php echo $row['title'] ?></td>
+ <td><?php echo $row['keywords'] ?></td>
+ <td><?php echo $row['author'] ?></td>
+ <td><?php echo date("Y-m-d H:i:s",$row['addtime']) ?></td>
+ <td><?php echo $row['content'] ?></td>
+ </tr>
+<?php }?>
+<tr>
+ <td colspan="6">
+<?php
+ echo " 當前{$page}/{$maxpage}頁 共{$totalnum}條";
+echo " <a href='searchpage.php?page=1{$url}'>首頁</a> ";
+echo "<a href='searchpage.php?page=".($page-1)."{$url}'>上一頁</a>";
+echo "<a href='searchpage.php?page=".($page+1)."{$url}'>下一頁</a>";
+echo " <a href='searchpage.php?page={$maxpage}{$url}'>尾頁</a> ";
+  
+?>
+</td>
+ </tr>
+</table>
