1d37d961bc313be14fbbe204bc88feb44e8e6fa4
leonachen <waterfay0.0@gmail.com>
2020-12-04 06:52:33 +0800

    cookie

--- LiuTr/invoicework/invoice/invoice_list.php
+++ LiuTr/invoicework/invoice/invoice_list.php
@@ -1,41 +1,76 @@
 <?php
 include_once "base.php";
-
+ini_set('display_errors','off'); 
 $period=ceil(date("m")/2);
 
-$sql="select * from `invoices` where period='$period' order by date desc";
+// $sql="select * from `invoices` where period='$period' order by date desc";
 
-$rows=$pdo->query($sql)->fetchAll();
+// $rows=$pdo->query($sql)->fetchAll();
 
-$m=ceil(date("m")/2);
-if(isset($_GET['pd'])){
-    $year=date('Y');
-    $period=$m;
-    $sql="select * from `invoices` where period='$period' order by date desc";
-}else{
-    $get_news=$pdo->query("SELECT * FROM `award_numbers` order by year desc ,period desc limit 1")->fetch();
-    $year=$get_news['year'];
-    $period=$get_news['period'];
-    }
-    if(isset($_GET['pd1'])){
-        $year=date('Y');
-        $period=$m-1;
-        $sql="select * from `invoices` where period='$period' order by date desc";
-        $rows=$pdo->query($sql)->fetchAll();
-    }
-    if(isset($_GET['pd2'])){
-        $year=date('Y');
-        $period=$m-2;
-        $sql="select * from `invoices` where period='$period' order by date desc";
-        $rows=$pdo->query($sql)->fetchAll();
-    }
+// $m=ceil(date("m")/2);
+// if(isset($_GET['pd'])){
+//     $year=date('Y');
+//     $period=$m;
+//     $sql="select * from `invoices` where period='$period' order by date desc";
+// }else{
+//     $get_news=$pdo->query("SELECT * FROM `award_numbers` order by year desc ,period desc limit 1")->fetch();
+//     $year=$get_news['year'];
+//     $period=$get_news['period'];
+//     }
+//     if(isset($_GET['pd1'])){
+//         $year=date('Y');
+//         $period=$m-1;
+//         $sql="select * from `invoices` where period='$period' order by date desc";
+//         $rows=$pdo->query($sql)->fetchAll();
+//     }
+//     if(isset($_GET['pd2'])){
+//         $year=date('Y');
+//         $period=$m-2;
+//         $sql="select * from `invoices` where period='$period' order by date desc";
+//         $rows=$pdo->query($sql)->fetchAll();
+//     }
+$ivname=$_POST['ivname'];
+$$keyword='keyword';
+$page=$_GET['page'];
+$count=ceil(COUNT($rows)/5);
+if(!empty($_POST['ivname']) || !empty('keyword') && empty($_GET['page'])){
+    setcookie("ivname",$_POST['ivname'],time()+3600);
+    setcookie("keyword",$_POST['keyword'],time()+3600);
+    $sql="SELECT * FROM `invoices` WHERE `code` LIKE '%{$_COOKIE[ivname]}%' && `number` LIKE '%{$_COOKIE['keyword']}%' LIMIT 0,5";
+    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+}
+else if(!empty($_GET['page'])){
+    $sql="SELECT * FROM `invoices` WHERE `code` LIKE '%{$_COOKIE[ivname]}%' && `number` LIKE '%{$_COOKIE['keyword']}%' LIMIT {$pagestart},{$pagesize}";
+    $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    for($i=1;$i<$count;$i++){
+    $pagestart=$pagestart+5;
+    $pagesize=$pagesize+5;
+}
+}
+
+
+// for($i=0;$i<$count;$i++){
+//     }
+ 
+    
+
+// 
+// echo COUNT($rows);
+// if(!empty($_GET['nextpag'])){
+//     $nextpage=$nextpage+5;
+// }else if(!empty($_GET['perpag']) && $prepage==0)
+// echo "搜尋數量:". COUNT(*);
 ?>
-<div class='row justify-content-around' style="list-style-type:none;paddin:0">
+<!-- <div class='row justify-content-around' style="list-style-type:none;paddin:0">
     <li><a href="?do=invoice_list&pd2=<?=$year?>-<?=$period?>">上上期發票</a></li>
     <li><a href="?do=invoice_list&pd1=<?=$year?>-<?=$period?>">上期發票</a></li>
     <li><a href="?do=invoice_list&pd=<?=$year?>-<?=$period?>">當期發票</a></li>
+</div> -->
+<form action="?do=invoice_list" method="post">
+<input type="text" name="ivname"><input type="text" name="keyword"><input type="submit" name="submit">
+</form>
 
-</div>
 <table class="table text-center">
     <tr>
         <td>發票號碼</td>
@@ -63,6 +98,21 @@ if(isset($_GET['pd'])){
         </td>
     </tr>
     <?php
-    }
+           }
+
+
     ?>
 </table>
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="?do=invoice_list&page=<?=$pagestart?>" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    <li class="page-item">
+      <a class="page-link" href="?do=invoice_list&page=<?=$pagesize?>" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
