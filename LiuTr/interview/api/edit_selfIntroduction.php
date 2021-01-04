25fcf3a649197736ced03853cdec31fea1ce4b7b
leonachen <waterfay0.0@gmail.com>
2021-01-04 16:09:01 +0800

    更新時刪除檔案

--- LiuTr/interview/api/edit_selfIntroduction.php
+++ LiuTr/interview/api/edit_selfIntroduction.php
@@ -6,14 +6,15 @@ include_once "../base.php";
 
 $SelfIntroduction=new DB('SelfIntroduction');
 $row=$SelfIntroduction->find(1);
+$delfile='../img/'.$row['img'];
 if(!empty($_FILES['img']['tmp_name'])){
+    unlink($delfile);
     $filename=$_FILES['img']['name'];
     move_uploaded_file($_FILES['img']['tmp_name'],'../img/'.$filename);
     $row['img']=$filename;
     $SelfIntroduction->save($row);
 }
 
-imagedestroy($im); 
 to("../backend/main.php?do=self_introduction");
 
 ?>
